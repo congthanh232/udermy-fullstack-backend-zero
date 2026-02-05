c90f0365ea0a9a13357e3ab31526ecc6e211c463
DESKTOP-P1ISDSL\ADMIN <thanhtche176384@fpt.edu.vn>
2026-02-05 23:49:36 +0700

    vid27

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const {getHomepage, getABC, getHoiDanIT} =require('../controllers/homeController')
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World!nodemon')
-})
+// router.method('/', handler)
 
-router.get('/abc', (req, res) => {
-  res.send('Check abc')
-})
+router.get('/', getHomepage)
 
-router.get('/hoidanit', (req, res) => {
-  // res.send('<h1> hoi dan it<h1>')
-  res.render('sample.ejs')
-})
+router.get('/abc', getABC)
+
+router.get('/hoidanit', getHoiDanIT)
 
 module.exports = router;
