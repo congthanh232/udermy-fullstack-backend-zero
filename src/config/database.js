ab4ca906c79bfc3dc476920f0095a9728a379bb9
DESKTOP-P1ISDSL\ADMIN <thanhtche176384@fpt.edu.vn>
2026-03-16 03:34:07 +0700

    fix supabase connection ipv4

--- src/config/database.js
+++ src/config/database.js
@@ -24,15 +24,9 @@ const mysql = require("mysql2/promise");
 // });
 
 // module.exports = connection;
-require("dotenv").config();
 const { Pool } = require("pg");
-
 const connection = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
