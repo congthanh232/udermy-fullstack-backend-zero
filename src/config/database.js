8253b5d421da56a2c8f9244c2f56bfbcca4cb265
DESKTOP-P1ISDSL\ADMIN <thanhtche176384@fpt.edu.vn>
2026-03-16 01:30:32 +0700

    fix database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const mysql = require('mysql2/promise')
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 
 // Create the connection to database
 // const connection = mysql.createConnection({
